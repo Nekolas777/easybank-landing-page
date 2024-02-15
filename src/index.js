45ce65753e4ecac8f37d27472e1f56310fd1889d
Nekolas777 <u202217485@upc.edu.pe>
2024-02-15 00:51:58 -0500

    navbar scroll effect and responsive components added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
+const header = document.getElementById('header');
 const hamburguerMenu = document.getElementById('nav-toggle');
 const nav = document.getElementById('nav-menu');
+const navLogo = document.querySelector('.nav__logo > img');
 
 hamburguerMenu.addEventListener('click', (e) => {
 
     console.log('u clicked hamburguer uwu');
     nav.classList.toggle('show-menu');
+
+})
+
+document.addEventListener('scroll', (e) => {
+
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+        navLogo.src = '/images/logo-footer.svg';
+    }
+    else {
+        header.classList.remove('scrolled');
+        navLogo.src = '/images/logo.svg';
+    }
+
+})
+
+document.addEventListener('DOMContentLoaded', (e) => {
+
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+        navLogo.src = '/images/logo-footer.svg';
+    }
 
 })
