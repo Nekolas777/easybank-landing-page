50b78ebddeadd3fc1fca36208fcf8f1ff338d942
Nekolas777 <u202217485@upc.edu.pe>
2024-02-16 02:35:33 -0500

    responsive features added

--- src/index.js
+++ src/index.js
@@ -1,33 +1,46 @@
+//const mainContent = document.getElementById('main');
 const header = document.getElementById('header');
 const hamburguerMenu = document.getElementById('nav-toggle');
+const toggleIcon = document.getElementById('toggle-icon');
 const nav = document.getElementById('nav-menu');
+
 const navLogo = document.querySelector('.nav__logo > img');
 
-hamburguerMenu.addEventListener('click', (e) => {
+function changesStateMenuClicked () {
 
     console.log('u clicked hamburguer uwu');
     nav.classList.toggle('show-menu');
+    document.body.classList.toggle('overlay');
+    toggleIcon.classList.toggle('fa-bars');
+    toggleIcon.classList.toggle('fa-x');
 
-})
+}
 
-document.addEventListener('scroll', (e) => {
+function ChangeStateScrolled (isScrolled) {
 
-    if (window.scrollY > 0) {
+    if (isScrolled) {
         header.classList.add('scrolled');
         navLogo.src = '/images/logo-footer.svg';
+        hamburguerMenu.style.color = '#FFF';
     }
     else {
         header.classList.remove('scrolled');
         navLogo.src = '/images/logo.svg';
+        hamburguerMenu.style.color = '#000';
     }
+
+}
+
+document.addEventListener('scroll', (e) => {
+
+    ChangeStateScrolled(window.scrollY > 0);
 
 })
 
 document.addEventListener('DOMContentLoaded', (e) => {
 
-    if (window.scrollY > 0) {
-        header.classList.add('scrolled');
-        navLogo.src = '/images/logo-footer.svg';
-    }
+    ChangeStateScrolled(window.scrollY > 0);
 
 })
+
+hamburguerMenu.addEventListener('click', changesStateMenuClicked)
